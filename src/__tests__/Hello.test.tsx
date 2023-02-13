76862c56d64a620a3206f304f446c94537265b8e
BaoNH <bao.nguyenh@saigontechnology.com>
2023-02-13 11:57:56 +0700

    fix: add @testing-library/react-native

--- src/__tests__/Hello.test.tsx
+++ src/__tests__/Hello.test.tsx
@@ -1,11 +1,41 @@
-/**
- * Created by NL on 04/01/2023.
- */
 import React from 'react'
-import renderer from 'react-test-renderer'
+import {render, fireEvent} from '@testing-library/react-native'
 import Hello from '../Hello'
 
-it('renders correctly with defaults', () => {
-  const button = renderer.create(<Hello name="World" enthusiasmLevel={1} />).toJSON()
-  expect(button).toMatchSnapshot()
+describe('Hello component', () => {
+  it('renders greeting text with given name and enthusiasm level', () => {
+    const name = 'John'
+    const enthusiasmLevel = 2
+    const {getByText} = render(<Hello name={name} enthusiasmLevel={enthusiasmLevel} />)
+    const greetingText = getByText(`Hello ${name}!!`)
+    expect(greetingText).toBeDefined()
+  })
+
+  it('throws an error if enthusiasm level is 0 or less', () => {
+    const name = 'John'
+    const enthusiasmLevel = 0
+    console.error = jest.fn()
+    render(<Hello name={name} enthusiasmLevel={enthusiasmLevel} />)
+    expect(console.error).toHaveBeenCalledWith('You could be a little more enthusiastic. :D')
+  })
+
+  it('increments enthusiasm level when the + button is pressed', () => {
+    const name = 'John'
+    const enthusiasmLevel = 2
+    const {getByText} = render(<Hello name={name} enthusiasmLevel={enthusiasmLevel} />)
+    const incrementButton = getByText('+')
+    fireEvent.press(incrementButton)
+    const greetingText = getByText(`Hello ${name}!!!`)
+    expect(greetingText).toBeDefined()
+  })
+
+  it('decrements enthusiasm level when the - button is pressed', () => {
+    const name = 'John'
+    const enthusiasmLevel = 2
+    const {getByText} = render(<Hello name={name} enthusiasmLevel={enthusiasmLevel} />)
+    const decrementButton = getByText('-')
+    fireEvent.press(decrementButton)
+    const greetingText = getByText(`Hello ${name}!`)
+    expect(greetingText).toBeDefined()
+  })
 })
