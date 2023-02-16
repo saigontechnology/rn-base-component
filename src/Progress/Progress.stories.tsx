b29b9ea9a5518dc8e5638a7b831bb02562448983
ngochuyduong <huy.duong@saigontechnology.com>
2023-02-16 14:05:50 +0700

    Add progress component

---
+++ src/Progress/Progress.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import type {ComponentMeta, ComponentStory} from '@storybook/react'
+
+import Progress from './Progress'
+
+export default {
+  title: 'components/Progress',
+  component: Progress,
+} as ComponentMeta<typeof Progress>
+
+export const Basic: ComponentStory<typeof Progress> = args => <Progress {...args} />
+
+Basic.args = {
+  value: 20,
+  filledTrackColor: '#49BE25',
+  backgroundColor: '#E5E5E5',
+  borderRadius: 8,
+}
---
+++ src/Progress/Progress.tsx
@@ -0,0 +1,84 @@
+import React, {useEffect, useRef, useState, forwardRef, memo} from 'react'
+import {View, StyleSheet, Animated, Dimensions} from 'react-native'
+
+interface IProgressProps {
+  /**
+   * Current percent of the Progress
+   * default 0, max 100
+   */
+  value: number
+  /**
+   * Defines height of Progress
+   * default 16
+   */
+  size?: number
+  /**
+   * Defines borderRadius of Progress
+   * default 0
+   */
+  borderRadius?: number
+  /**
+   * Defines color of Track Bar
+   * default #49BE25
+   */
+  filledTrackColor?: string
+  /**
+   * Defines background color of Progress
+   * default #E5E5E5
+   */
+  backgroundColor?: string
+}
+
+const {width: screenWidth} = Dimensions.get('screen')
+
+const Progress = (props: IProgressProps, ref?: any) => {
+  const {
+    value = 0,
+    size = 16,
+    borderRadius = 0,
+    filledTrackColor = '#49BE25',
+    backgroundColor = '#E5E5E5',
+  } = props
+  const [width, setWidth] = useState(0)
+  const translateX = useRef(new Animated.Value(-screenWidth)).current
+  const toTranslateXTo = useRef(new Animated.Value(-screenWidth)).current
+
+  useEffect(() => {
+    Animated.timing(translateX, {
+      toValue: toTranslateXTo,
+      duration: 500,
+      useNativeDriver: true,
+    }).start()
+  }, [])
+
+  useEffect(() => {
+    const _value = value >= 100 ? 100 : value
+    toTranslateXTo.setValue(-width + (width * _value) / 100)
+  }, [width, value])
+
+  return (
+    <View
+      ref={ref}
+      style={StyleSheet.flatten([styles.progress, {backgroundColor, height: size, borderRadius}])}
+      onLayout={({nativeEvent: {layout}}) => setWidth(layout.width)}>
+      <Animated.View
+        style={{
+          transform: [{translateX}],
+          backgroundColor: filledTrackColor,
+          borderRadius,
+          height: size,
+          width,
+        }}
+      />
+    </View>
+  )
+}
+
+export default memo(forwardRef(Progress))
+
+const styles = StyleSheet.create({
+  progress: {
+    width: '100%',
+    overflow: 'hidden',
+  },
+})
