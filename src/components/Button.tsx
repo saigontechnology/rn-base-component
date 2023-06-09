5b9000decccb692f696589fff5f7deadd4b0ee92
loido <117077260+loido@users.noreply.github.com>
2023-06-09 15:26:06 +0700

    chore: add base theme (#32)
    * chore: add base theme
    
    * chore: update default theme
    
    * chore: update button component
    
    * chore: update base theme
    
    * chore: update define name sizes
    
    * chore: update opacity name
    
    * chore: update name component
    
    ---------
    
    Co-authored-by: loidev <nhoxbaycao@gmail.com>

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import {metrics} from '../helpers/metrics'
-import type {ITheme} from '../theme'
 
 export type ButtonProps = {
   onPress: () => void
@@ -11,26 +10,28 @@ export type ButtonProps = {
 }
 
 const Button: React.FC<ButtonProps> = ({text, onPress, color, textColor}) => (
-  <ButtonRoot onPress={onPress} activeOpacity={0.8} color={color}>
+  <ButtonWrapper onPress={onPress} activeOpacity={0.8} color={color}>
     <Label color={textColor}>{text}</Label>
-  </ButtonRoot>
+  </ButtonWrapper>
 )
 
-const ButtonRoot = styled.TouchableOpacity`
-  ${(props: {theme: ITheme; color: string | undefined}) => ({
-    paddingVertical: metrics.xxs,
-    paddingHorizontal: metrics.small,
-    borderRadius: metrics.borderRadius,
-    backgroundColor: props?.color || props?.theme?.colors?.cardPrimaryBackground,
-    alignSelf: 'flex-start',
-  })}
-`
+interface IButtonWrapper {
+  color?: string
+}
+const ButtonWrapper = styled.TouchableOpacity<IButtonWrapper>(({theme, color}) => ({
+  paddingVertical: metrics.xxs,
+  paddingHorizontal: metrics.small,
+  borderRadius: metrics.borderRadius,
+  backgroundColor: color || theme.colors.green,
+  alignSelf: 'flex-start',
+}))
 
-const Label = styled.Text`
-  ${(props: {theme: ITheme; color: string | undefined}) => ({
-    color: props?.color || 'white',
-    fontWeight: props?.theme?.fontWeights?.bold,
-  })}
-`
+interface ILabel {
+  color?: string
+}
+const Label = styled.Text<ILabel>(({theme, color}) => ({
+  color: color || 'white',
+  fontWeight: theme?.fontWeights?.bold,
+}))
 
 export default Button
--- type.ts
+++ type.ts
@@ -1,1 +1,6 @@
-declare module 'rn-base-component'
+declare module 'rn-base-component' {
+  export * from 'src/components'
+  export * from 'src/core'
+  export * from 'src/hooks'
+  export * from 'src/theme'
+}
