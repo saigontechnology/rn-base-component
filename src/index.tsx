100a5f2fa4970f32d07dfbed805b98e18eb5a70a
loido <loi.do@saigontechnology.com>
2023-06-13 10:48:52 +0700

    chore: fix eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import {requireNativeComponent, UIManager, Platform, ViewStyle} from 'react-native'
 import type {ITheme} from './theme'
 
