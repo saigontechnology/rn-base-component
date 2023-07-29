c4694000ab0b6b1ef2b56aaae229fd30a2442f31
loido <117077260+loido@users.noreply.github.com>
2023-07-29 15:57:41 +0700

    Merge pull request #41
    * chore: export component
    
    * chore: fix eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import {requireNativeComponent, UIManager, Platform, ViewStyle} from 'react-native'
-import Button from './components/Button'
 import type {ITheme} from './theme'
 
 declare module 'styled-components/native' {
@@ -23,8 +23,6 @@ export const RnBaseComponentView =
     : () => {
         throw new Error(LINKING_ERROR)
       }
-
-export const RnBaseButton = Button
 
 export * from './components'
 export * from './core'
