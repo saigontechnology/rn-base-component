027fe9102e93ca8b50297521691103f0f19d043e
ledutu <111717882+ledutu2@users.noreply.github.com>
2023-08-18 15:44:02 +0700

    feat: add card (#36)
    Co-authored-by: HuyDo <>

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import Button from './Button'
 import Progress from './Progress/Progress'
 import Checkbox from './Checkbox/Checkbox'
 import CodeInput from './CodeInput/CodeInput'
+import Card from './Card/Card'
 
-export {Button, CodeInput, Checkbox, Progress, RadioButton}
+export {Button, CodeInput, Checkbox, Progress, RadioButton, Card}
 export * from './Text/Text'
