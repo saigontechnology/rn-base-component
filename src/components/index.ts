310f23b6089e8ee881d346a1494a9c016c7efbc0
ngochuyduong <huy.duong@saigontechnology.com>
2023-09-13 13:44:27 +0700

    chore: fix duplicate export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
-import Button from './Button'
 import RadioButton from './RadioButton/RadioButton'
+import Button from './Button'
 
+import Progress from './Progress/Progress'
 import Checkbox from './Checkbox/Checkbox'
 import CodeInput from './CodeInput/CodeInput'
+import Slider from './Slider/Slider'
+import Card from './Card/Card'
+import TextInput from './TextInput/TextInput'
 import HighlightText from './HighlightText/HighlightText'
-import Progress from './Progress/Progress'
 
+export {Button, CodeInput, Checkbox, Progress, Slider, RadioButton, Card, TextInput, HighlightText}
 export * from './Text/Text'
-export {Button, Checkbox, CodeInput, HighlightText, Progress, RadioButton, HighlightText}
-
