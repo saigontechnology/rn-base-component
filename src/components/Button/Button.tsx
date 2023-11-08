c068ec127ded09ba4866bba37c29e1d548127170
chung.nguyenh <chung.nguyenh@saigontechnology.com>
2023-11-08 18:10:55 +0700

    fix: wrong style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -112,7 +112,7 @@ const ButtonWrapper = styled.TouchableOpacity(
     rightIcon,
   }: Omit<ButtonProps, 'text' | 'onPress'> & {theme?: ITheme}) => ({
     paddingVertical: theme?.spacing.small,
-    fexDirection: 'row',
+    flexDirection: 'row',
     paddingHorizontal: theme?.spacing.slim,
     borderRadius,
     backgroundColor: disabled ? theme?.colors.muted : backgroundColor || theme?.colors.green,
