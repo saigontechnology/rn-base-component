d133981ec5caa504ed3e1179dc080b373d978b65
chung.nguyenh <chung.nguyenh@saigontechnology.com>
2023-11-21 14:19:29 +0700

    fix: remove blank line

--- src/components/CodeInput/CodeInput.tsx
+++ src/components/CodeInput/CodeInput.tsx
@@ -117,7 +117,6 @@ const CodeInput: React.FC<CodeInputProps> = ({
         return <PlaceholderText color={placeholderTextColor}>{placeholder ?? ''}</PlaceholderText>
       }
     },
-
     [
       renderCursor,
       secureTextEntry,
