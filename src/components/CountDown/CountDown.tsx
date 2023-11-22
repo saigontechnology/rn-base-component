dcbd740645fcb3fd5433297b914054752f07ceb4
ZanBin <150883191+ZanBin@users.noreply.github.com>
2023-11-22 09:10:41 +0700

    Update CountDown.tsx

--- src/components/CountDown/CountDown.tsx
+++ src/components/CountDown/CountDown.tsx
@@ -118,7 +118,6 @@ export const CountDown: React.FunctionComponent<CountDownProps> = ({
   )
 
   const renderTimer = () => {
-    const numberSecondPerMinute = 60
     /*
     caculate minutes
     */
