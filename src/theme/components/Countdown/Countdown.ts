697ed90a19240dafe7dfc35250590fa443f42a86
ZanBin <150883191+ZanBin@users.noreply.github.com>
2023-11-22 08:59:55 +0700

    Update Countdown.ts

--- src/theme/components/Countdown/Countdown.ts
+++ src/theme/components/Countdown/Countdown.ts
@@ -2,7 +2,7 @@ import base from '../../base'
 import {metrics} from '../../../helpers'
 import type {CountDownProps} from '../../../components/CountDown/CountDown'
 
-export type CountDownThemeProps = {} & Pick<CountDownProps, 'textStyle'>
+export type CountDownThemeProps = Pick<CountDownProps, 'textStyle'>
 
 export const CountDownTheme: CountDownThemeProps = {
   textStyle: {
