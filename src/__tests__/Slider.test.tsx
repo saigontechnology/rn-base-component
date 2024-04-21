d067e28c823bd7c55bdce328ea24678eec5a628f
baonguyenh-allio <bao.nguyenh@alliofinance.com>
2024-04-21 11:41:59 +0700

    fix: update job name in on_pr_develop.yml

--- src/__tests__/Slider.test.tsx
+++ src/__tests__/Slider.test.tsx
@@ -15,33 +15,33 @@ it.todo('write a test')
 //     expect(sliderComponent).toBeDefined()
 //   })
 
-  // test('calls onValueChange when sliding', () => {
-  //   const onValueChangeMock = jest.fn()
-  //   const {getByTestId} = render(<Slider onValueChange={onValueChangeMock} />)
+// test('calls onValueChange when sliding', () => {
+//   const onValueChangeMock = jest.fn()
+//   const {getByTestId} = render(<Slider onValueChange={onValueChangeMock} />)
 
-  //   // Simulate a slide event (you may need to adjust the coordinates)
-  //   fireEvent.apply(getByTestId('slider'), {
-  //     nativeEvent: {changedTouches: [{locationX: 50}]},
-  //   })
+//   // Simulate a slide event (you may need to adjust the coordinates)
+//   fireEvent.apply(getByTestId('slider'), {
+//     nativeEvent: {changedTouches: [{locationX: 50}]},
+//   })
 
-  //   // Ensure onValueChange has been called
-  //   expect(onValueChangeMock).toHaveBeenCalled()
-  // })
+//   // Ensure onValueChange has been called
+//   expect(onValueChangeMock).toHaveBeenCalled()
+// })
 
-  // test('show correct track point', () => {
-  //   const {getByTestId} = render(<Slider showTrackPoint={true} sliderWidth={100} />)
-  //   const trackPoints = getByTestId('track-point')
-  //   expect(trackPoints).toBeDefined()
-  // })
+// test('show correct track point', () => {
+//   const {getByTestId} = render(<Slider showTrackPoint={true} sliderWidth={100} />)
+//   const trackPoints = getByTestId('track-point')
+//   expect(trackPoints).toBeDefined()
+// })
 
-  // test('handles tapToSeek properly', () => {
-  //   const onValueChangeMock = jest.fn()
-  //   const {getByTestId} = render(<Slider tapToSeek onValueChange={onValueChangeMock} />)
+// test('handles tapToSeek properly', () => {
+//   const onValueChangeMock = jest.fn()
+//   const {getByTestId} = render(<Slider tapToSeek onValueChange={onValueChangeMock} />)
 
-  //   // Simulate a tap event (you may need to adjust the coordinates)
-  //   fireEvent.press(getByTestId('slider'))
+//   // Simulate a tap event (you may need to adjust the coordinates)
+//   fireEvent.press(getByTestId('slider'))
 
-  //   // Ensure onValueChange has been called
-  //   expect(onValueChangeMock).toHaveBeenCalled()
-  // })
+//   // Ensure onValueChange has been called
+//   expect(onValueChangeMock).toHaveBeenCalled()
+// })
 // })
