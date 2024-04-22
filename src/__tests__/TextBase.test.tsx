431fff217c786462f2c64305249ea312fae28415
ThinhKimVo <thinh.vo@saigontechnology.com>
2024-04-22 14:27:33 +0700

    fix: upgrade react-native-gesture-handle Pan Gesture
    Signed-off-by: ThinhKimVo <thinh.vo@saigontechnology.com>

--- src/__tests__/TextBase.test.tsx
+++ src/__tests__/TextBase.test.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import {render} from '@testing-library/react-native'
 import {Text, TextBold, TextItalic} from '../components'
+import {BaseProvider} from '../core'
+
+const renderElement = (Component: React.ReactElement) => render(<BaseProvider>{Component}</BaseProvider>)
 
 describe('Text', () => {
   it('renders Text correctly', () => {
-    const {getByText} = render(<Text>Hello, World!</Text>)
+    const {getByText} = renderElement(<Text>Hello, World!</Text>)
     expect(getByText('Hello, World!')).toBeDefined()
   })
 
   it('renders TextBold correctly', () => {
-    const {getByText} = render(<TextBold>Hello, Bold Text!</TextBold>)
+    const {getByText} = renderElement(<TextBold>Hello, Bold Text!</TextBold>)
     expect(getByText('Hello, Bold Text!')).toBeDefined()
   })
 
   it('renders TextItalic correctly', () => {
-    const {getByText} = render(<TextItalic>Hello, Italic Text!</TextItalic>)
+    const {getByText} = renderElement(<TextItalic>Hello, Italic Text!</TextItalic>)
     expect(getByText('Hello, Italic Text!')).toBeDefined()
   })
 })
