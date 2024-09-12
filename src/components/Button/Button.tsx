f9d0803a93e5ed5d18dc27cd84f481ca56e33896
HoangDT <hoang.dinhtien@saigontechnology.com>
2024-09-12 13:10:30 +0700

    fix: button height

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -95,7 +95,7 @@ export const Button: React.FC<ButtonProps> = ({
       outlineWidth={outlineWidth}
       borderRadius={borderRadius ?? ButtonTheme.borderRadius}
       disabled={disabled}
-      style={[{minHeight: ButtonTheme.height}, StyleSheet.flatten(style)]}
+      style={[{height: ButtonTheme.height}, StyleSheet.flatten(style)]}
       {...props}>
       {!!leftIcon && leftIcon}
       {typeof children === 'string' ? (
