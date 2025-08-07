fb40960bcb8b22e3fe83576b19db41df78f620fb
Hoàng Phi Hồng <hong.hoang@saigontechnology.com>
2025-08-07 09:53:57 +0700

    release 0.5.0 (#110)
    * fix: ensure radio button is circular on all devices (#108)
    
    * release 0.5.0 (#109)
    
    * chore: add publish config
    
    * v0.0.11
    
    * chore: release 0.0.12
    
    * chore: bump version
    
    * chore: release 0.1.0
    
    * feat: refactor component (#105)
    
    * chore: change prepare
    
    * chore: change prepare
    
    * chore: change prepare
    
    * fix: button height
    
    * feat: add custom prop to radio buton
    
    * chore: fix script for release package
    
    ---------
    
    Co-authored-by: Hong Hoang <hong.hoang@saigontechnology.com>
    Co-authored-by: HoangDT <hoang.dinhtien@saigontechnology.com>
    Co-authored-by: hang.nguyen <hang.nguyen@saigontechnology.com>
    
    * chore: release 0.2.0
    
    * fix: text not recognize type (#106)
    
    * fix: text not recognize type
    
    * fix: on change of code input
    
    * fix: fix comments
    
    * fix: add styled text italic
    
    ---------
    
    Co-authored-by: HuyDo <huydo@saigontechnology.com>
    Co-authored-by: DanhMC <danh.mai@saigontechnology.com>
    
    * feat: enhance CodeInput component with improved quality and documentation (#107)
    
    Co-authored-by: hong.hoang <honghp95@gmail.com>
    
    * chore: release 0.3.0
    
    * feat: add focusCellWrapperStyle
    
    * chore: release 0.4.0
    
    ---------
    
    Co-authored-by: Hong Hoang <honghp95@gmail.com>
    Co-authored-by: loc-nguyent-sts <114563576+loc-nguyenthien@users.noreply.github.com>
    Co-authored-by: HoangDT <hoang.dinhtien@saigontechnology.com>
    Co-authored-by: hang.nguyen <hang.nguyen@saigontechnology.com>
    Co-authored-by: huydosgtech <105196859+huydosgtech@users.noreply.github.com>
    Co-authored-by: HuyDo <huydo@saigontechnology.com>
    Co-authored-by: DanhMC <danh.mai@saigontechnology.com>
    
    ---------
    
    Co-authored-by: Đinh Tiên Hoàng <117052298+hoangSTS@users.noreply.github.com>
    Co-authored-by: Hong Hoang <honghp95@gmail.com>
    Co-authored-by: loc-nguyent-sts <114563576+loc-nguyenthien@users.noreply.github.com>
    Co-authored-by: HoangDT <hoang.dinhtien@saigontechnology.com>
    Co-authored-by: hang.nguyen <hang.nguyen@saigontechnology.com>
    Co-authored-by: huydosgtech <105196859+huydosgtech@users.noreply.github.com>
    Co-authored-by: HuyDo <huydo@saigontechnology.com>
    Co-authored-by: DanhMC <danh.mai@saigontechnology.com>

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -116,7 +116,7 @@ export const RadioButton = forwardRef<View, IRadioButtonProps>(
 
     const outer = useMemo(
       () => ({
-        width: responsiveWidth(outerSize),
+        width: responsiveHeight(outerSize),
         height: responsiveHeight(outerSize),
         border: responsiveHeight(outerSize / 2),
       }),
@@ -124,7 +124,7 @@ export const RadioButton = forwardRef<View, IRadioButtonProps>(
     )
     const inner = useMemo(
       () => ({
-        width: responsiveWidth(innerSize),
+        width: responsiveHeight(innerSize),
         height: responsiveHeight(innerSize),
         border: responsiveHeight(innerSize / 2),
       }),
